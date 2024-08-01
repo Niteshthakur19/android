ff59cd1f4cc999d6c053a3766859aafc6bf0846b
Niteshthakur19 <nt93248@gmail.com>
2024-08-01 14:46:01 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,90 +1,24 @@
-package com.example.myapplication;
+package com.example.myapplication02;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textViewOutput;
-    private String input = "";
-    private double num1 = 0;
-    private double num2 = 0;
-    private String operator = "";
-    private boolean hasDecimal = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        textViewOutput = findViewById(R.id.textViewOutput);
-    }
-
-    // Method to handle digit button clicks
-    public void onDigitClick(View view) {
-        Button button = (Button) view;
-        String digit = button.getText().toString();
-        input += digit;
-        textViewOutput.setText(input);
-    }
-
-    // Method to handle decimal button click
-    public void onDecimalClick(View view) {
-        if (!hasDecimal) {
-            input += ".";
-            textViewOutput.setText(input);
-            hasDecimal = true;
-        }
-    }
-
-    // Method to handle operator button clicks (+, -, *, /)
-    public void onOperatorClick(View view) {
-        Button button = (Button) view;
-        operator = button.getText().toString();
-        num1 = Double.parseDouble(input);
-        input = "";
-        textViewOutput.setText("0");
-        hasDecimal = false;
-    }
-
-    // Method to handle clear (C) button click
-    public void onClearButtonClick(View view) {
-        input = "";
-        num1 = 0;
-        num2 = 0;
-        operator = "";
-        textViewOutput.setText("0");
-        hasDecimal = false;
-    }
-
-    // Method to handle equals (=) button click
-    public void onEqualsClick(View view) {
-        num2 = Double.parseDouble(input);
-        double result = 0;
-
-        switch (operator) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                result = num1 / num2;
-                break;
-        }
-
-        textViewOutput.setText(String.valueOf(result));
-        input = String.valueOf(result);
-        num1 = result;
-        num2 = 0;
-        operator = "";
-        hasDecimal = false;
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
